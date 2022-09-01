f5a857699fcf32993fb624f2210fe7d4c507f4f2
nikhila <nkmp@simcorp.com>
2022-09-01 12:16:57 +0200

    task 1 done

--- Homework-1/Program.cs
+++ Homework-1/Program.cs
@@ -5,8 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Homework_1 {
+    enum TransactionType { Buy, Sell }
     class Program {
         static void Main(string[] args) {
+
+            Console.WriteLine("Input the nominal of the trade:");
+            string userInput = Console.ReadLine();
+            int nominal = Int32.Parse(userInput);
+
+            Console.WriteLine("Input the tradeprice");
+            userInput = Console.ReadLine();
+            double tradePrice = Double.Parse(userInput) ;
+
+            TransactionType trcType;
+            Console.WriteLine("Input the transaction type");
+            userInput = Console.ReadLine();
+            trcType = (TransactionType)Enum.Parse(typeof(TransactionType), userInput, true);
+
+            double currentValue = 0.0d;
+            if (trcType == TransactionType.Buy)
+                currentValue = nominal * tradePrice;
+            else 
+                 currentValue = nominal * tradePrice*-1;
+
+            Console.WriteLine($" The Current Value is:{currentValue}");
+            Console.ReadKey();
+
+
         }
     }
 }
